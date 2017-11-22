b8ae6fce34f9c2f976232119927c2ce04d8df3fb
Daniel Perez <tuvistavie@gmail.com>
2017-11-22 11:33:22 +0900

    Add get_config to DenseMulti

--- suplearn_clone_detection/layers.py
+++ suplearn_clone_detection/layers.py
@@ -202,6 +202,22 @@ class DenseMulti(Layer):
         output_shape[-1] = self.units
         return tuple(output_shape)
 
+    def get_config(self):
+        config = {
+            'units': self.units,
+            'activation': activations.serialize(self.activation),
+            'use_bias': self.use_bias,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer),
+            'bias_initializer': initializers.serialize(self.bias_initializer),
+            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+            'activity_regularizer': regularizers.serialize(self.activity_regularizer),
+            'kernel_constraint': constraints.serialize(self.kernel_constraint),
+            'bias_constraint': constraints.serialize(self.bias_constraint)
+        }
+        base_config = super(DenseMulti, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
 
 custom_objects = {
     "SplitInput": SplitInput,
