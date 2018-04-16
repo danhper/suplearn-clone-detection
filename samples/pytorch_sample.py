ad56faf170f4a701655383ef5531b891d5037179
Daniel Perez <tuvistavie@gmail.com>
2018-04-16 14:28:31 +0900

    Fix lstm

--- samples/pytorch_sample.py
+++ samples/pytorch_sample.py
@@ -1,11 +1,16 @@
 from os import path
 
+import numpy as np
+
+import torch
+from torch.autograd import Variable
+
 from suplearn_clone_detection.ast_loader import ASTLoader
 from suplearn_clone_detection.config import Config
 from suplearn_clone_detection import ast_transformer
 from suplearn_clone_detection.pytorch_model import Encoder
 
-ast_paths = path.expanduser("~/workspaces/research/dataset/atcoder/asts/asts.json")
+ast_paths = path.expanduser("~/workspaces/research/dataset/atcoder/asts/asts-small.jsonl")
 input_files = ["src/b/4/0/1381956.java", "src/b/22/0/1133780.java"]
 
 
@@ -19,3 +24,7 @@ loader = ASTLoader(ast_paths)
 sample_asts = [trans.transform_ast(loader.get_ast(v)) for v in input_files]
 
 encoder = Encoder(lang_config)
+
+model_input = Variable(torch.from_numpy(np.array(sample_asts, dtype=np.long)))
+output = encoder(model_input)
+print(output.data.shape)
--- suplearn_clone_detection/pytorch_model.py
+++ suplearn_clone_detection/pytorch_model.py
@@ -12,12 +12,17 @@ class Encoder(nn.Module):
         self.config = config
         self._build_embedding()
         self.lstm = nn.LSTM(
-            input_size=self.vocab_size,
+            input_size=self.config.embeddings_dimension,
             hidden_size=config.output_dimensions[0],
             num_layers=len(config.output_dimensions),
             bidirectional=config.bidirectional_encoding,
             batch_first=True,
         )
+        self.hidden = None
+
+    def reset(self):
+        self.zero_grad()
+        self.hidden = None
 
     def _build_embedding(self):
         self.vocab_size = self.config.vocabulary_size
@@ -36,6 +41,7 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         x = self.embeddings(x)
-        x = self.lstm(x)
+        x, self.hidden = self.lstm(x)
+        x = x[:, -1, :]
 
         return x
